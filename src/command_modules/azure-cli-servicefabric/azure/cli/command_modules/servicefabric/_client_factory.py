4a463da6600caddaa2feb889caed1490a41fb443
Ching Chen <chingc@microsoft.com>
2017-07-11 22:17:50 -0700

    fix

--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_client_factory.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_client_factory.py
@@ -3,32 +3,12 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-def servicefabric_client_factory(**_):
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    from azure.mgmt.servicefabric import ServiceFabricManagementClient
-    return get_mgmt_service_client(ServiceFabricManagementClient)
-
-def resource_client_factory(**_):
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    from azure.mgmt.resource import ResourceManagementClient
-    return get_mgmt_service_client(ResourceManagementClient)
 
 def keyvault_client_factory(**_):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azure.mgmt.keyvault import KeyVaultManagementClient
     return get_mgmt_service_client(KeyVaultManagementClient)
 
-def compute_client_factory(**_):
-    from azure.cli.core.profiles import ResourceType
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    return get_mgmt_service_client(ResourceType.MGMT_COMPUTE)
-
-def storage_client_factory(**_):
-    from azure.cli.core.profiles import ResourceType
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    return get_mgmt_service_client(ResourceType.MGMT_STORAGE)
-
-def network_client_factory(**_):
-    from azure.cli.core.profiles import ResourceType
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    return get_mgmt_service_client(ResourceType.MGMT_NETWORK)
+	
+def keyvault_client_vaults_factory(kwargs):
+    return keyvault_client_factory(**kwargs).vaults
--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/commands.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/commands.py
@@ -9,41 +9,21 @@
 from azure.cli.core.commands import cli_command
 from azure.cli.core.util import empty_on_404
 
-from ._client_factory import servicefabric_client_factory
+from ._client_factory import keyvault_client_factory,keyvault_client_vaults_factory
+
+
+data_client_path = 'azure.keyvault.key_vault_client#{}'
 
 custom_path = 'azure.cli.command_modules.servicefabric.custom#{}'
-mgmt_path = 'azure.mgmt.servicefabric.operations.clusters_operations#{}'
 
-factory = lambda args: servicefabric_client_factory(**args).clusters
+# cli_command(__name__, 'sbzrm load', custom_path.format('load'), keyvault_client_vaults_factory)
 
-cli_command(__name__, 'sf1 cluster show', mgmt_path.format('ClustersOperations.get'), factory, exception_handler=empty_on_404)
+# cli_command(__name__, 'sbzrm load', custom_path.format('load'), keyvault_client_vaults_factory)
 
-cli_command(__name__, 'sf1 cluster list', custom_path.format('list_cluster'), factory)
+cli_command(__name__, 'sbzrm application get', custom_path.format('get'), keyvault_client_vaults_factory)
 
-cli_command(__name__, 'sf1 cluster new', custom_path.format('new_cluster'), factory, no_wait_param=False)
+cli_command(__name__, 'sbzrm application create', custom_path.format('put'), keyvault_client_vaults_factory)
 
-cli_command(__name__, 'sf1 cluster certificate add', custom_path.format('add_cluster_cert'), factory, no_wait_param=False)
+cli_command(__name__, 'sbzrm application update', custom_path.format('post'), keyvault_client_vaults_factory)
 
-cli_command(__name__, 'sf1 cluster certificate remove', custom_path.format('remove_cluster_cert'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster client certificate add', custom_path.format('add_client_cert'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster client certificate remove', custom_path.format('remove_client_cert'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster setting set', custom_path.format('set_cluster_setting'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster setting remove', custom_path.format('remove_cluster_setting'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster reliability-level update', custom_path.format('update_cluster_reliability_level'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster node-type durability set', custom_path.format('update_cluster_durability'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster node-type add', custom_path.format('add_cluster_node_type'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster node-type node add', custom_path.format('add_cluster_node'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster node-type node remove', custom_path.format('remove_cluster_node'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 cluster upgrade-type set', custom_path.format('update_cluster_upgrade_type'), factory, no_wait_param=False)
-
-cli_command(__name__, 'sf1 application certificate add', custom_path.format('add_app_cert'), factory, no_wait_param=False)
+cli_command(__name__, 'sbzrm application delete', custom_path.format('delete'), keyvault_client_vaults_factory)
